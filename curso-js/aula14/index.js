ca9f3aa21a8482588ccba4143d2030c60ec84aeb
João Vitor <126678195+jvodev@users.noreply.github.com>
2024-01-01 12:08:07 -0300

    Add files via upload

---
+++ curso-js/aula14/index.js
@@ -0,0 +1,14 @@
+let num1 = 1500;
+let num2 = 2.5;
+
+//console.log(num1.toString() + num2);
+
+// num1 = num1.toString() // ficaria eternamente como string
+
+console.log(typeof num1);
+
+console.log(num1.toString(2));
+console.log(num1.toFixed(2));
+
+let temp = num1 * 'Ola';
+console.log(temp);
